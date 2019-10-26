512a2789e16fc516bf888d6db631a3153c96558a
Brian Longmore <43781990+branogarbo@users.noreply.github.com>
2019-10-26 00:45:46 -1000

    Trying to get qs and functions to work.

--- hotelscript.js
+++ hotelscript.js
@@ -1,1 +1,51 @@
-var rooms = new Object();
+function qs(sel) {
+  return document.querySelector(sel);
+}
+
+function test(rom) {
+  console.log(qs(`#${rom} input`).value);
+}
+
+function flash(sel) {
+  setTimeout(()=>{sel.style = "color:yellow;"}, 60);
+  setTimeout(()=>{sel.style = "color:red;"}, 120);
+  setTimeout(()=>{sel.style = "color:yellow;"}, 180);
+  setTimeout(()=>{sel.style = "color:red;"}, 240);
+  setTimeout(()=>{sel.style = "color:yellow;"}, 300);
+  setTimeout(()=>{sel.style = "color:red;"}, 360);
+}
+
+var room1 = new Object();
+var room2 = new Object();
+var room3 = new Object();
+var room4 = new Object();
+var room5 = new Object();
+var room6 = new Object();
+
+room1.guests = [];
+room1.iscleaning = false;
+
+room2.guests = [];
+room2.cleaning = false;
+
+room3.guests = [];
+room3.cleaning = false;
+
+room4.guests = [];
+room4.cleaning = false;
+
+room5.guests = [];
+room5.cleaning = false;
+
+room6.guests = [];
+room1.cleaning = false;
+
+function checkin(room) {
+  if (room.guests.length == 4) {
+    flash(qs(`#${room} .bttn1`));
+  }
+  else {
+    room.guests.push(qs(`#${room} input`).value);
+    console.log(room.guests);
+  }
+}
