740babeaaa64dac39b034f408a8acc6c5c5103b6
Brian Longmore <43781990+branogarbo@users.noreply.github.com>
2019-11-24 00:57:58 -1000

    Completed locking system.

--- hotelscript.js
+++ hotelscript.js
@@ -8,7 +8,7 @@ for (i=1;i<=6;i++) {
   eval(`
     var room${i} = new Object();
     room${i}.guests = [];
-    room${i}.cleaning = false;
+    room${i}.locked = false;
   `);
 }
 
@@ -17,15 +17,13 @@ function qs(sel) {
 }
 
 function flash(sel) {
-  var originalcolor = window.getComputedStyle(qs(sel),null).getPropertyValue('color');
-  
   for (i=0;i<3;i++) {
     eval(`
-    setTimeout(()=>{qs(sel).style = "color:yellow;"}, ${120*i+60});
-    setTimeout(()=>{qs(sel).style = "color:red;"}, ${120*(i+1)});
+      setTimeout(()=>{qs(sel).style = "color:yellow;"}, ${120*i+60});
+      setTimeout(()=>{qs(sel).style = "color:red;"}, ${120*(i+1)});
     `);
   }
-  setTimeout(()=>{qs(sel).style = `color:${originalcolor};`}, 420);
+  setTimeout(()=>{qs(sel).style = `color:#31393C`}, 420);
 }
 
 function addel(position,element,content) {
@@ -57,12 +55,14 @@ function check(room) {
 }
 
 function checkin(room) {
-  if (eval(room).guests.length == 4 || eval(room).cleaning == true) {
+  if (eval(room).guests.length == 4) {
     flash(`#${room} .checkbttn`);
+    qs(`#${room} input`).placeholder = "Enter number to check out";
   }
   else {
     eval(room).guests.push(qs(`#${room} input`).value);
     addel(`#${room} ol`,'li',qs(`#${room} input`).value);
+    qs(`#${room} input`).placeholder = "Guests appear below";
     masterlog.push([room,qs(`#${room} input`).value,currtime()]);
   }
   console.clear();
@@ -74,25 +74,41 @@ function checkout(room) {
     eval(room).guests = [];
     remel(`#${room} ol`);
     addel(`#${room}`,'ol','');
+    qs(`#${room} input`).placeholder = "Check guests here";
   }
   else if (qs(`#${room} input`).value < 1 || qs(`#${room} input`).value > eval(room).guests.length || eval(room).guests.length == 0)  {
     flash(`#${room} .checkbttn`);
+    qs(`#${room} input`).placeholder = "Check guests here";
   }
   else {
     eval(room).guests.splice(qs(`#${room} input`).value-1,1);
     remel(`#${room} li:nth-of-type(${qs(`#${room} input`).value})`);
+    if (eval(room).guests.length == 3) {
+      qs(`#${room} input`).placeholder = 'Enter "$all" to clear room';
+    }
   }
 }
 
-room2.cleaning = true;
-
-function roomclean(room) {
-  if (eval(room).cleaning == false) {
-    eval(room).cleaning = true;
-    qs(`#${room}`).style = "background:#f1f4f8;";
+function roomlock(room) {
+  if (eval(room).guests.length != 0) {
+    qs(`#${room} img`).style = "animation-play-state:running";
+    setTimeout(()=>{qs(`#${room} img`).style = "animation-play-state:paused";},390);
+  }
+  else if (eval(room).locked == false) {
+    eval(room).locked = true;
+    qs(`#${room}`).style = "background:#ff9bab;";
+    qs(`#${room} img`).src = "locked-padlock.svg";
+    qs(`#${room} input`).placeholder = "This room is locked!";
+    qs(`#${room} input`).style = "background:#ffffff;";
+    qs(`#${room} input`).disabled = true;
+    qs(`.checkbttn`).disabled = true;
   }
   else {
-    eval(room).cleaning = false;
-    qs(`#${room}`).style = "background:#ff9bab;";
+    eval(room).locked = false;
+    qs(`#${room}`).style = "background:#e0e7f0;";
+    qs(`#${room} img`).src = "unlocked-padlock.svg";
+    qs(`#${room} input`).placeholder = "Check guests here";
+    qs(`#${room} input`).disabled = false;
+    qs(`.checkbttn`).disabled = false;
   }
 }
